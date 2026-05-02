3003f1ab3a3eadd40461c54f7011cda1d9dce518
hardarik1 <hardarik1@yandex.ru>
2026-05-02 19:00:38 +0300

    Добавлен вызов калькулятора в Program.cs

--- Program.cs
+++ Program.cs
@@ -6,5 +6,6 @@ class HelloGit
     {
         Console.WriteLine("Привет, Git-сервис!");
         Console.WriteLine($"Дата: {DateTime.Now:dd.MM.yyyy}");
+        Console.WriteLine($"3 + 5 = {Calculator.Add(3, 5)}");
     }
 }
